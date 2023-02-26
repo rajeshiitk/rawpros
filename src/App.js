d79c84b154a52244bfd31975ab582d1d8f328a7a
Rajesh Choudhary <rajuvert18@gmail.com>
2023-02-26 19:03:23 +0530

    sidebar added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import './styles/App.scss';
 import './styles/Header.scss';
+import './styles/Home.scss';
 
 
 function App() {
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='side'>
+        <h1>RFQ List</h1>
+        <div>
+          <h3>Sort By :</h3>
+          <div>
+            <input type="checkbox" name="pulished" id="" />
+            <span>
+              <label htmlFor="published">Published Date</label>
+              <input type="date" name="" id="" />
+            </span>
+          </div>
+          <div>
+            <input type="checkbox" name="closing" id="" />
+            <span>
+              <label htmlFor="closing">Closing Date</label>
+              <input type="date" name="" id="" />
+            </span>
+          </div>
+        </div>
+        <h3>Preferred Source of Origin</h3>
+      </div>
+      <div className="main">
+        main
+      </div>
+    </div>
   )
 }
 
